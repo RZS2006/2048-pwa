3125a705cca0254cffb6b86112c5e0ddb7d196e7
Raiyan Shamim <razash139@gmail.com>
2021-05-22 19:09:54 +0300

    js selectors

--- js/app.js
+++ js/app.js
@@ -1,1 +1,30 @@
-// JS
+// Selectors
+
+const navMenuLink = document.querySelector('#nav__menu-link');
+const navGameLink = document.querySelector('#nav__game-link');
+const navBestScore = document.querySelector('#nav__best-score');
+
+const menuSection = document.querySelector('#menu-section');
+const menuBaseNumberSelect = document.querySelector(
+	'#menu__base-number-select'
+);
+const menuNewGameBtn = document.querySelector('#menu__new-game-btn');
+
+const gameOverSection = document.querySelector('#game-over-section');
+const gameOverResult = document.querySelector('#game-over__result');
+const gameOverScore = document.querySelector('#game-over__score');
+const gameOverMoves = document.querySelector('#game-over__moves');
+const gameOverKeepPlayingBtn = document.querySelector(
+	'#game-over__keep-playing-btn'
+);
+const gameOverPlayAgainBtn = document.querySelector(
+	'#game-over__play-again-btn'
+);
+
+const gameGrid = document.querySelector('#game__grid');
+const gameScore = document.querySelector('#game__score');
+const gameMoves = document.querySelector('#game__moves');
+const gameLeftBtn = document.querySelector('game__left-btn');
+const gameUpBtn = document.querySelector('game__up-btn');
+const gameDownBtn = document.querySelector('game__down-btn');
+const gameRightBtn = document.querySelector('game__right-btn');
